75020d0b7e8d7ef1d3a62aeadd7cf234bffed451
Arsen <arsproger@gmail.com>
2023-03-09 23:02:32 +0600

    init

--- src/main/java/com/arsen/springpaymentmanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/arsen/springpaymentmanagementsystem/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.arsen.springpaymentmanagementsystem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -25,13 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                     .withUser("user")
-                    .password("{noop}user")
+                    .password("user")
                     .roles("USER")
                 .and()
                     .withUser("admin")
-                    .password("{noop}admin")
+                    .password("admin")
                     .roles("ADMIN");
     }
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
 
